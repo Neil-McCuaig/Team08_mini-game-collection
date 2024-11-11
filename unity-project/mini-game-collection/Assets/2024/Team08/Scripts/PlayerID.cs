e0e1cceded82dcfdaca127fb2c87e1ee941af755
Neil-McCuaig <Neil_McCuaig@bell.net>
2024-11-11 17:50:42 -0500

    Created a basic scene
    Created a scene for our minigame, with a flat plain, two players, and a test cube for seeing testing our painting script.

---
+++ unity-project/mini-game-collection/Assets/2024/Team08/Scripts/PlayerID.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MiniGameCollection.Games2024.Team08
+{
+    public class PlayerID : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
