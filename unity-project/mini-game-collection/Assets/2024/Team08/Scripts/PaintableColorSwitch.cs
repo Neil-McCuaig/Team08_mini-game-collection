c8b819f620a12f343147c42bbfa07451ccbb9c9e
Neil-McCuaig <Neil_McCuaig@bell.net>
2024-11-12 14:49:49 -0500

    Set up Coloring code
    Set up code for coloring the cubes and making them identify on the back end as "Red" or "Blue" so we can implement scoring later. Also made sure collisions won't launch the players or cubes.

---
+++ unity-project/mini-game-collection/Assets/2024/Team08/Scripts/PaintableColorSwitch.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiniGameCollection.Games2024.Team08
+{
+    public class PaintableColorSwitch : MonoBehaviour
+    {
+        //[SerializeField] public Color switchColor = Color.white;
+
+        [SerializeField] public bool IsNeutral;
+        [SerializeField] public bool IsRed;
+        [SerializeField] public bool IsBlue;
+        [SerializeField] public Material[] Material;
+        Renderer rend;
+        // Start is called before the first frame update
+        void Start()
+        {
+            IsNeutral = true;
+            IsRed = false;
+            IsBlue = false;
+            rend = GetComponent<Renderer>();
+            rend.enabled = true;
+            rend.sharedMaterial = Material[0];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            bool redCollor = other.GetComponentInChildren<RedPlayer>() != null;
+            bool blueCollor = other.GetComponentInChildren<BluePlayer>() != null;
+            //Debug.Log("Is it colliding");
+            if (redCollor && IsNeutral == true)
+            {
+                IsRed = true;
+                IsNeutral = false;
+                rend.sharedMaterial = Material[1];
+            }
+            else if (blueCollor && IsNeutral == true)
+            {
+                IsBlue = false;
+                rend.sharedMaterial = Material[2];
+            }
+            else if (redCollor && IsBlue == true) 
+            {
+                IsBlue = false;
+                IsRed = true;
+                rend.sharedMaterial = Material[1];
+            }
+            else if (blueCollor && IsRed == true)
+            {
+                IsRed = false;
+                IsBlue = true;
+                rend.sharedMaterial = Material[2];
+            }
+        }
+    }
+}
+
